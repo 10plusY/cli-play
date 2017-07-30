4b86d8af446f703e0596b3b3b1867c11aa166b2b
10plusY <grigsmusic1990@gmail.com>
2017-07-30 13:22:53 -0400

    little helpz; comms

--- cli.py
+++ cli.py
@@ -2,7 +2,11 @@ from cmd import Cmd
 
 import os
 
+def get_prompt_string():
+    return '(Folder: {})'.format(os.path.basename(os.getcwd()))
+
 class CliPlay(Cmd):
+    # Init
     def __init__(self):
         """Shell params set here"""
 
@@ -10,21 +14,27 @@ class CliPlay(Cmd):
 
         self.intro = 'WELCOME TO CLI-PLAY\n'
 
+    # Attrs
     def set_prompt(self, prompt):
         self.prompt = prompt
 
-    def get_prompt(self):
-        return '(Folder: {}) '.format(os.path.basename(os.getcwd()))
-
+    # Hooks
     def preloop(self):
-        self.set_prompt(self.get_prompt())
+        self.set_prompt(get_prompt_string())
 
+    def postloop(self):
+        pass
+
+
+    # Do Methods
     def do_quit(self, args):
         """Generic quit function for now."""
 
         print "Shutting down..."
 
         raise SystemExit
+
+    # Help Methods
 
 if __name__ == '__main__':
     play = CliPlay()
