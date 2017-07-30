323d54f4fd1d70e60aa542ec385358cb8f1eec92
10plusY <grigsmusic1990@gmail.com>
2017-07-29 23:42:29 -0400

    prompty class methods

--- cli.py
+++ cli.py
@@ -1,13 +1,23 @@
 from cmd import Cmd
 
+import os
+
 class CliPlay(Cmd):
     def __init__(self):
         """Shell params set here"""
 
         Cmd.__init__(self)
 
-        self.intro = 'Welcome '
-        self.prompt = '|> '
+        self.intro = 'WELCOME TO CLI-PLAY\n'
+
+    def set_prompt(self, prompt):
+        self.prompt = prompt
+
+    def get_prompt(self):
+        return '(Folder: {}) '.format(os.path.basename(os.getcwd()))
+
+    def preloop(self):
+        self.set_prompt(self.get_prompt())
 
     def do_quit(self, args):
         """Generic quit function for now."""
