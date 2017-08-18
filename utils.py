70bd57e8864bc38af86a68fd07ff73bc44b4a4cd
10plusY <grigsmusic1990@gmail.com>
2017-08-18 08:15:32 -0700

    walk function wrappers

--- utils.py
+++ utils.py
@@ -3,6 +3,23 @@ import os
 from collections import deque
 
 def walk_level(path, level):
+    """
+    Walks the file system up to a certain level/depth
+
+    i.e. For a directory tree
+
+    -- /
+        -- Foo
+        -- Bar
+            -- Qux
+                -- Quux
+                -- Corge
+
+    walk_level('/', 2) returns ('/', '/Foo', '/Bar', '/Bar/Qux')
+
+    :param path - path to form the root of the tree
+    :param level - number of steps to go down the tree
+    """
     path = os.path.abspath(path)
 
     current_depth = path.count(os.path.sep)
@@ -14,3 +31,20 @@ def walk_level(path, level):
 
         if current_depth + level <= root_depth:
             del dirs[:]
+
+def walk_down(level):
+    """
+    Walks downwards to certain level
+
+    :param level - number of steps to go down the tree
+    """
+    return walk_level('.', level)
+
+def walk_up(level):
+    """
+    Walks downwards but first moves the root of the tree
+    a certain level up
+
+    :param level - number of steps to go down the tree
+    """
+    return walk_level('/'.join(['..'] * level), level)
